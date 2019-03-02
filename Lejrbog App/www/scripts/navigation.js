eaf951e8c472b7b3b68a7c40f106f0c27607de8f
Casper Hansen <mctoast.dk@gmail.com>
2019-03-02 22:44:46 +0100

    Formatting

--- Lejrbog App/www/scripts/navigation.js
+++ Lejrbog App/www/scripts/navigation.js
@@ -1,22 +1,22 @@
 function toggleNav() {
-    if ($(".sidenav").css("width") > "0px") {
-        $(".sidenav").css("width", "0em");
-    } else {
-        $(".sidenav").css("width", "15.625em");
-    }
+	if ($(".sidenav").css("width") > "0px") {
+		$(".sidenav").css("width", "0em");
+	} else {
+		$(".sidenav").css("width", "15.625em");
+	}
 }
 
 $(setTimeout(setMargin, 100));
 
 function setMargin() {
-    var margin = $('.icon-bar').css('height');
-    $('.sidenav').css('margin-top', margin);
+	var margin = $('.icon-bar').css('height');
+	$('.sidenav').css('margin-top', margin);
 }
 
 $(document).mouseup(function (e) {
-    var el = $(".sidenav");
+	var el = $(".sidenav");
 
-    if (!el.is(e.target) && el.has(e.target).length === 0) {
-        el.css("width", "0em");
-    }
+	if (!el.is(e.target) && el.has(e.target).length === 0) {
+		el.css("width", "0em");
+	}
 });
