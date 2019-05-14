33b85e29368346acfa0fadc3b568ccc1aa54dc4c
davidwiljac <david.wilson.jacobsen@gmail.com>
2019-05-14 16:59:46 +0200

    copy/pasted info for fact, kontigent, korps

--- Lejrbog App/www/scripts/logbog.js
+++ Lejrbog App/www/scripts/logbog.js
@@ -1,23 +1,28 @@
 function onLoad() {
 	dataText = JSON.parse(localStorage.getItem("dataText"));
-	dataNums = JSON.parse(localStorage.getItem("dataNums"));
-
+    
 	for (var i = 0; i < 11; i++) {
 		var dag = document.getElementById("dag0").cloneNode(true);
 		dag.children[0].innerHTML = "Dag " + (i + 2);
 		dag.id = "dag" + (i + 1);
 		document.getElementById("main").appendChild(dag);
+        if(i == 2){
+            var ele = document.createElement("h2");
+            var text = document.createTextNode("Lejren");
+            ele.appendChild(text);
+            document.getElementById("main").appendChild(ele);
+        }
 	}
 
 	if (dataText != null) {
 		var dag = document.getElementById("dag0").cloneNode(true);
-		dag.childNodes[5].value = dataText[0];
+		dag.childNodes[3].value = dataText[0];
 		for (var i = 0; i < 12; i++) {
-			document.getElementById("dag" + i).childNodes[5].value = dataText[i];
+			document.getElementById("dag" + i).childNodes[3].value = dataText[i];
 
 		}
 	}
-
+/*
 	if (dataNums != null) {
 		var dag = document.getElementById("dag0").cloneNode(true);
 		dag.childNodes[3].value = dataNums[0];
@@ -25,14 +30,14 @@ function onLoad() {
 			document.getElementById("dag" + i).childNodes[3].value = dataNums[i];
 
 		}
-	}
+	}*/
 
 }
 
 function saveReflection() {
 	var dataText = [];
-	for (var i = 0; i < 12; i++) {
-		dataText[i] = document.getElementById("dag" + i).childNodes[5].value;
+    for (var i = 0; i < 12; i++) {
+		dataText[i] = document.getElementById("dag" + i).childNodes[3].value;
 	}
 	localStorage.setItem("dataText", JSON.stringify(dataText));
 }
